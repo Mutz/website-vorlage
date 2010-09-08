3b4423feae0dbb710de4419a9ad21b6e7f89cd44
Stefan Imhoff <stefan.imhoff@duf.de>
2010-09-08 17:02:21 +0200

    Umstellung auf COMPASS

---
+++ config.rb
@@ -0,0 +1,8 @@
+http_path = "/"
+css_dir = "css"
+sass_dir = "src"
+images_dir = "css/img"
+javascripts_dir = "js"
+output_style = :compact # :nested, :expanded, :compact, :compressed
+line_comments = false
+relative_assets = true
