5e1572752f3d9a37dcf619ba1f05fa7fec78ba50
Stefan Imhoff <stefan@imhoff.name>
2010-10-25 15:37:40 +0200

    GoogleMaps API3

--- js/script.js
+++ js/script.js
@@ -1,63 +1,61 @@
 (function($){
 
     /* Google-Maps */
-    if (GBrowserIsCompatible()) {
-        var m = $("#map")[0];
-        if (m) {
-            // Mittelpunkt der Karte
-            var map = new GMap2(m);
-            
-            // Position (Länge, Breite)
-            var point = new GLatLng(53.555045414405996,9.995241165161133);
-            
-            // Bedienelemente für Zoom und Pan anzeigen
-            map.setMapType(G_NORMAL_MAP); /* 2D-Karte */
-            // map.setMapType(G_HYBRID_MAP); /* Satelllitenbild & Straßen */
-            // map.setMapType(G_SATELLITE_MAP); /* Satellitenaufnahme */
-            // map.setMapType(G_PHYSICAL_MAP); /* Gelände */
-            map.setUIToDefault(); /* Standard-Navigation */
-            // map.addControl(new GOverviewMapControl()); /* Übersichtskarte einblenden */
-            // map.addControl(new GScaleControl()); /* Massstab einblenden */
-            // map.addControl(new GLargeMapControl()); /* Große Navigation (alt) */
-            // map.addControl(new GSmallMapControl()); /* Kleine Navigation (alt) */
-            // map.addControl(new GMapTypeControl()); /* Karte, Satellite, Hybrid (alt) */
-            map.enableScrollWheelZoom();
-            map.enableContinuousZoom(); 
+    initialize();
 
-            // Icon
-            var MyIcon = new GIcon(G_DEFAULT_ICON);
-            MyIcon.image = "css/img/googlemaps/pin.png";
-            //MyIcon.shadow = "css/img/googlemaps/pin_shadow.png";
-            //MyIcon.shadowSize=new GSize(19,19);
-            MyIcon.iconSize = new GSize(20, 34);
-            MyIcon.iconWindowAnchor = new GPoint(10, 10);
-
-            // GMarkerOptions Objekt
-            markerOptions = { icon:MyIcon };
-            var inactiveMirror = new GIcon(G_DEFAULT_ICON);
-            var activeMirror = new GIcon(G_DEFAULT_ICON);
-
-            // Vergrösserungsfaktor: 
-            // 0: Welt
-            // 1: Halbkugel
-            // [...]
-            // 16: Ein paar Straßenzüge
-            // 20: Maximal
-            var zoomLevel = 15;
-
-            // Zentrum setzen
-            map.setCenter(point, zoomLevel);
-
-            // Markierung hineinpieksen
-            map.addOverlay(marker = new GMarker(point,MyIcon)); 
-
-            // Infofenster anhängen (HTML)
-            GEvent.addListener(marker, 'click', function() {
-                marker.openInfoWindowHtml("<strong>###FIRMENNAME###</strong><br />###STRASSE###<br />###PLZ ORT###");
-            });
-        }
-    }
-    
 })(window.jQuery);
 
 
+function initialize() {
+    var latlng = new google.maps.LatLng(53.555045414405996, 9.995241165161133);
+    var myOptions = {
+        center: latlng,
+        zoom: 15,
+        scaleControl: true,
+        disableDefaultUI: false,
+        mapTypeId: google.maps.MapTypeId.ROADMAP, // SATELLITE, HYBRID, TERRAIN
+        mapTypeControl: true,
+        mapTypeControlOptions: {
+            style: google.maps.MapTypeControlStyle.DROPDOWN_MENU, // HORIZONTAL_BAR, DEFAULT
+            position: google.maps.ControlPosition.TOP_RIGHT, // TOP, TOP_RIGHT, LEFT, RIGHT, BOTTOM_LEFT, BOTTOM, BOTTOM_RIGHT
+        },
+
+        navigationControl: true,
+        navigationControlOptions: {
+            style: google.maps.NavigationControlStyle.SMALL, // ZOOM_PAN, ANDROID, DEFAULT
+        }
+    };
+
+    var map = new google.maps.Map(document.getElementById("map"), myOptions);
+
+    // var image = 'css/img/googlemaps/pin.png';
+    var image = new google.maps.MarkerImage('css/img/googlemaps/pin.png',
+            new google.maps.Size(20, 34),
+            new google.maps.Point(0, 0),
+            new google.maps.Point(10, 34));
+
+    var shadow = new google.maps.MarkerImage('css/img/googlemaps/shadow-pin.png',
+            new google.maps.Size(20,34),
+            new google.maps.Point(0, 0),
+            new google.maps.Point(0, 32));
+
+    var marker = new google.maps.Marker({
+        position: latlng,
+        icon: image
+    });
+
+    var contentString = '<div id="mapcontent">' +
+        '<h2>####</h2>' +
+        '<p>###</p>' +
+        '</div>';
+
+    var infowindow = new google.maps.InfoWindow({
+        content: contentString
+    });
+
+    google.maps.event.addListener(marker, 'click', function(){
+        infowindow.open(map, marker);
+    });
+
+    marker.setMap(map);
+}
